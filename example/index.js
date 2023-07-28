0bc2e3668909165d73dad08aa9357b59fc3899e2
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-07-28 18:28:48 -0400

    chore: deleted example folder

--- example/index.js
+++
@@ -1,9 +0,0 @@
-const { githubContributions } = require('../src/index')
-
-githubContributions('Unnunoctio')
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => {
-    console.error(err)
-  })
